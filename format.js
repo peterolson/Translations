769fa4b5dfcbaa6140ac8132b470561f32798c90
Peter Olson <peter.e.c.olson+github@gmail.com>
2015-09-30 16:07:52 +0800

    Show page title

--- format.js
+++ format.js
@@ -1,4 +1,6 @@
 (function() {
+	var headings = document.getElementsByTagName("h1");
+	document.title = headings[0].innerHTML + " " + headings[1].innerHTML;
 	var chapters = document.getElementsByTagName("td");
 	for(var i = 0; i < chapters.length; i++) {
 		var chapter = chapters[i];
